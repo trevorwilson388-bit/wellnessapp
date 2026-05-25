8dc92c49ead00255678a5dbde25d1671b9d05b4f
Trevor <trevorwilson388@gmail.com>
2026-05-25 11:58:23 +0300

    Update Hydration_alert with water tracking counter

--- app/src/main/java/com/example/tembotrevor/Hydration_alert.kt
+++ app/src/main/java/com/example/tembotrevor/Hydration_alert.kt
@@ -1,12 +1,19 @@
 package com.example.tembotrevor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Hydration_alert : AppCompatActivity() {
+
+    private var waterGlasses = 0
+    private val dailyGoal = 8 // Recommended 8 glasses per day
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,37 @@ class Hydration_alert : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val countText = findViewById<TextView>(R.id.waterCountText)
+        val progressBar = findViewById<ProgressBar>(R.id.waterProgressBar)
+        val addWaterButton = findViewById<Button>(R.id.addWaterButton)
+        val resetButton = findViewById<Button>(R.id.resetWaterButton)
+        val statusText = findViewById<TextView>(R.id.waterStatusText)
+
+        progressBar.max = dailyGoal
+
+        fun updateDisplay() {
+            countText.text = "$waterGlasses / $dailyGoal glasses"
+            progressBar.progress = waterGlasses
+            when {
+                waterGlasses == 0 -> statusText.text = "Start tracking your water intake!"
+                waterGlasses < dailyGoal -> statusText.text = "${dailyGoal - waterGlasses} more glasses to go!"
+                waterGlasses == dailyGoal -> statusText.text = "Great! You've met your daily goal!"
+                else -> statusText.text = "Amazing! You're over your goal!"
+            }
+        }
+
+        updateDisplay()
+
+        addWaterButton.setOnClickListener {
+            waterGlasses++
+            updateDisplay()
+        }
+
+        resetButton.setOnClickListener {
+            waterGlasses = 0
+            updateDisplay()
         }
     }
 }
